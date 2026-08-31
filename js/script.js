5bbde287d4ee79e4122a092a1c1723f4202284a9
Jose-MP-Gouveia <jmpgouveia05@duck.com>
2026-08-31 17:57:10 +0100

    Adding button again

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-document.querySelector("h1").addEventListener("click", function() {
-    alert("You clicked the heading!");
+document.querySelector("#myButton").addEventListener("click", function() {
+    alert("You clicked the button!");
 });
